0637bd6f31d046cbb65f6168b4f0df31342f091e
lebichloan <lebichloan2512@gmail.com>
2023-11-01 19:36:56 +0700

    step add bill

---
+++ QLNS/Pages/AddBillStep5.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QLNS.Pages
+{
+    /// <summary>
+    /// Interaction logic for AddBillStep5.xaml
+    /// </summary>
+    public partial class AddBillStep5 : Page
+    {
+        public AddBillStep5()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNext_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnPreious_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
