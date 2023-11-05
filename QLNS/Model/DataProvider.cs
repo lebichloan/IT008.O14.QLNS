a9599573a587ce3695b3e7fe3b61947540a90243
Bui Thai Hoang <127731981+BTH260704@users.noreply.github.com>
2023-11-05 13:06:07 +0700

    xử lí đăng nhập

---
+++ QLNS/Model/DataProvider.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLNS.Model
+{
+    public class DataProvider
+    {
+        public static DataProvider _ins;
+        public static DataProvider Ins
+        {
+            get
+            {
+                if (_ins == null)
+                    _ins = new DataProvider();
+                return _ins;
+            }
+            set
+            {
+                _ins = value;
+            }
+        }
+        public QLNSEntities DB { get;set; }
+        private DataProvider()
+        {
+            DB = new QLNSEntities();
+        }
+    }
+}
