7d11c2256a997f1adb028b592ba22267c42515f3
Bui Thai Hoang <127731981+BTH260704@users.noreply.github.com>
2023-12-05 14:16:15 +0700

    UPdateStaff

---
+++ QLNS/ResourceXAML/MessageOption.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace QLNS.ResourceXAML
+{
+    /// <summary>
+    /// Interaction logic for MessageOption.xaml
+    /// </summary>
+    public partial class MessageOption : Window
+    {
+        public static bool isAgree;
+        public MessageOption()
+        {
+            InitializeComponent();
+            isAgree = false;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void btnNo_Click(object sender, RoutedEventArgs e)
+        {
+            isAgree = false;
+            Close();
+        }
+
+        private void btnYes_Click(object sender, RoutedEventArgs e)
+        {
+            isAgree = true;
+            Close();
+        }
+    }
+}
